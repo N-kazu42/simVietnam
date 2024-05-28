ab6ca794fddeb8df651a9e3bf63926cf16eb8666
N-kazu42 <shioatsupapakazuaki@gmail.com>
2024-05-29 07:56:47 +0900

    main スライド時間変更

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ style.innerHTML = `
   }
 }
 
+.f-Rom {
+  font-family: "Rampart One", sans-serif;
+}
+
+.f-Noto-S {
+  font-family: "Noto Sans JP", sans-serif;
+}
+
+.f-Zen {
+  font-family: "Zen Antique", serif;
+}
+
 .slide {
   background-size: 100%;
   background-position: center;
@@ -18,13 +30,13 @@ style.innerHTML = `
 }
 
 .slide.active {
-  animation: zoomIn 3s ease-in-out forwards;
+  animation: zoomIn 6s ease-in-out forwards;
 }
 
 .fade-up {
   opacity: 0;
   transform: translateY(20px);
-  transition: opacity 1s ease-out, transform 1s ease-out;
+  transition: opacity 6s ease-out, transform 6s ease-out;
 }
 
 .fade-up.visible {
@@ -35,6 +47,23 @@ style.innerHTML = `
 
 // Append the style to the head
 document.head.appendChild(style);
+
+// Add Google Fonts
+const link1 = document.createElement('link');
+link1.rel = 'preconnect';
+link1.href = 'https://fonts.googleapis.com';
+document.head.appendChild(link1);
+
+const link2 = document.createElement('link');
+link2.rel = 'preconnect';
+link2.href = 'https://fonts.gstatic.com';
+link2.crossOrigin = 'anonymous';
+document.head.appendChild(link2);
+
+const link3 = document.createElement('link');
+link3.href = 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;200;300;400;500;600;700;800;900&family=Rampart+One&family=Zen+Antique&display=swap';
+link3.rel = 'stylesheet';
+document.head.appendChild(link3);
 
 // JavaScript for handling the slides and fade-up animation
 document.addEventListener("DOMContentLoaded", function () {
@@ -86,7 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   showSlide(currentSlide);
-  setInterval(nextSlide, 3000); // 3秒ごとにスライドを切り替える
+  setInterval(nextSlide, 6000); // 3秒ごとにスライドを切り替える
 
   // Fade-up animation logic
   const fadeUpElements = document.querySelectorAll('.fade-up');
